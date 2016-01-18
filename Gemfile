8f0c6e1b626a15181e4568070a8a883a8ec59491
tjlee@woowahan.com <tjlee@woowahan.com>
2016-01-18 18:46:13 +0900

    test

--- Gemfile
+++ Gemfile
@@ -47,5 +47,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'responders', '~> 2.0'
-
-
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,7 +2,10 @@ class ShopsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   respond_to :html, :json
+  before_action :set_shop, only: [:show, :edit, :update, :destory]
 
+  # GET /shops
+  # GET /shops.json
   def index
     @shops = Shop.all
     @mainregion = params[:mainregion]
@@ -14,11 +17,38 @@ class ShopsController < ApplicationController
     end
   end
 
+  # GET /shops/1
+  # GET /shops/1.json
   def show
     @shop = Shop.find(params[:id])
+  end
+
+  # GET /shops/new
+  def new
+    @shop = Shop.new
+  end
+
+  # GET /shops/1/edit
+  def edit
+  end
+
+  # POST /shops
+  # POST /shops.json
+  def create
+    @shop = Shop.new(shop_params)
   end
 
   def not_found
     head status: :not_found
   end
+
+  private
+
+    def set_shop
+      @shop = Shop.find(params[:id])
+    end
+
+    def shop_params
+      params.require(:shop).permit(:shopowner_id, :name, :phone, :start_time, :end_time, :holiday, :address, :main_image)
+    end
 end
